d6f916ed2aa0f0c6376eb3015a4ac05bb7d62ff9
ManuelNvro <manosnavarro@gmail.com>
2019-12-16 11:24:06 -0500

    Added RunAll routine

--- Scripts/PythonRunAllDymola.py
+++ Scripts/PythonRunAllDymola.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[12]:
+# In[1]:
 
 
 import platform
@@ -12,7 +12,6 @@ import pandas as pd
 import numpy as np
 import os
 import shutil
-from subprocess import Popen
 
 
 # In[ ]:
@@ -21,15 +20,60 @@ from subprocess import Popen
 #This is intended to be used in the manuelnvro Dell using Dymola 2020
 
 
-# In[13]:
+# In[2]:
 
 
 #Run Exciters
-print('Exciters Testing ----------------------------------------')
+print('---------------------------------------------------------- Exciters Testing ----------------------------------------------------------')
 try:
-    os.chdir(f"/home/manuelnvro/dev/Gitted/PythonTesting/Scripts/")
-    os.system('PythonExcitersDymola.py')
+    os.chdir(f"/home/manuelnvro/dev/Gitted/PythonTesting/Scripts")
+    exec(open("PythonExcitersDymola.py").read())
     print('Exciters Testing OK...')
 except:
-    print('Error in Exciters Testing')
+    print('Error in Exciters Testing...')
+
+
+# In[3]:
+
+
+#Run Machines
+print('---------------------------------------------------------- Machines Testing ----------------------------------------------------------')
+try:
+    os.chdir(f"/home/manuelnvro/dev/Gitted/PythonTesting/Scripts")
+    exec(open("PythonMachinesDymola.py").read())
+    print('Exciters Testing OK...')
+except:
+    print('Error in Exciters Testing...')
+
+
+# In[4]:
+
+
+#Run Turbine Governors
+print('---------------------------------------------------------- Turbine Governors Testing ----------------------------------------------------------')
+try:
+    os.chdir(f"/home/manuelnvro/dev/Gitted/PythonTesting/Scripts")
+    exec(open("PythonTurbineGovernorsDymola.py").read())
+    print('Turbine Governors Testing OK...')
+except:
+    print('Error in Turbine Governors Testing...')
+
+
+# In[5]:
+
+
+#Run Power System Stabilizer
+print('---------------------------------------------------------- Power System Stabilizers Testing ----------------------------------------------------------')
+try:
+    os.chdir(f"/home/manuelnvro/dev/Gitted/PythonTesting/Scripts")
+    exec(open("PythonPowerSystemStabilizerDymola.py").read())
+    print('Power System Stabilizer Testing OK...')
+except:
+    print('Error in Power System Stabilizer Testing...')
+
+
+# In[ ]:
+
+
+print('---------------------------------------------------------- End of All Simulations ----------------------------------------------------------')
 
